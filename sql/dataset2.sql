4ab1c8506c196dea92fa29535834cffaf266ee3b
testmapper <67954166+testmapper-official@users.noreply.github.com>
2023-12-25 22:29:54 +0400

    fix update-script

--- sql/dataset2.sql
+++ sql/dataset2.sql
@@ -23,41 +23,16 @@ ALTER TABLE diabet_predict.dataset2
 ADD COLUMN ID INT AUTO_INCREMENT PRIMARY KEY FIRST;
 
 INSERT INTO diabet_predict.gender(ID, name)
-SELECT ID, gender
-FROM dataset2;
-
-ALTER TABLE diabet_predict.gender
-ADD COLUMN genderID INT PRIMARY KEY;
-UPDATE diabet_predict.gender
-    SET genderID = 1
-    WHERE name = 'Male';
-UPDATE gender
-    SET genderID = 2
-    WHERE name = 'Female';
+SELECT ROW_NUMBER() OVER (ORDER BY gender ASC) as ID, gender as name
+FROM (SELECT DISTINCT gender FROM dataset2) as dataset2;
 
 INSERT INTO diabet_predict.smokinghistory(ID, info)
-SELECT ID, smoking_history
-FROM dataset2;
-
-ALTER TABLE diabet_predict.smokinghistory
-ADD COLUMN skID INT PRIMARY KEY;
-UPDATE diabet_predict.smokinghistory
-    SET skID = 1
-    WHERE info = 'never';
-UPDATE diabet_predict.smokinghistory
-    SET skID = 2
-    WHERE info = 'not current'; 
-UPDATE diabet_predict.smokinghistory
-    SET skID = 3
-    WHERE info = 'No info';
-UPDATE diabet_predict.smokinghistory
-    SET skID = 4
-    WHERE info = 'ever';
-UPDATE diabet_predict.smokinghistory
-    SET skID = 5
-    WHERE info = 'former';  
+SELECT ROW_NUMBER() OVER (ORDER BY smoking_history ASC) as ID, smoking_history as info
+FROM (SELECT DISTINCT smoking_history FROM dataset2) as dataset2;
 
 INSERT INTO diabet_predict.bloodanalysis
-( SELECT ds.ID, gend.genderID, sh.skID, ds.age, ds.bmi, ds.HbA1c_level, ds.blood_glucose_level, ds.heart_disease, ds.hypertension, ds.diabetes
+( SELECT ds.ID, gend.ID as genderID, sh.ID as shID, ds.age, ds.bmi, ds.HbA1c_level, ds.blood_glucose_level, ds.heart_disease, ds.hypertension, ds.diabetes
 FROM dataset2 as ds, smokingHistory as sh, Gender as gend
-WHERE sh.info = ds.smoking_history AND gend.name = ds.gender )
+WHERE sh.info = ds.smoking_history AND gend.name = ds.gender );
+
+DROP TABLE IF EXISTS dataset2;

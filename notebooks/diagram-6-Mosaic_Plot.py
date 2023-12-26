ea50c17355f8761b9bbac32efc8d93f30058fa84
zhiznenyj <1xxx163xxx1@gmail.com>
2023-12-26 16:42:52 +0400

    Добавил улучшенную мозаичную диаграмму

--- notebooks/diagram-6-Mosaic_Plot.py
+++ notebooks/diagram-6-Mosaic_Plot.py
@@ -4,13 +4,20 @@ import plotly.express as px
 # Загрузка данных из CSV
 df = pd.read_csv('diabetes_prediction_dataset.csv')
 
-# Данные для визуализации
-grouped_data = df.groupby(['gender', 'age', 'diabetes']).size().reset_index(name='counts')
+# Заменяем 0 и 1 на более понятные обозначения
+df['diabetes'] = df['diabetes'].map({0: 'No Diabetes', 1: 'Diabetes'})
+df['heart_disease'] = df['heart_disease'].map({0: 'No Heart Disease', 1: 'Heart Disease'})
+df['hypertension'] = df['hypertension'].map({0: 'No Hypertension', 1: 'Hypertension'})
 
-# Создание мозаичной диаграммы
-fig = px.treemap(grouped_data, path=['gender', 'age', 'diabetes'], values='counts',
-                 color='diabetes',
+# Агрегация всех данных для визуализации
+grouped_data = df.groupby(['gender', 'age', 'diabetes', 'heart_disease', 'hypertension']).size().reset_index(name='counts')
+
+# Создание мозаичной диаграммы с дополнительными условиями
+fig = px.treemap(grouped_data, path=['gender', 'age', 'diabetes', 'heart_disease', 'hypertension'], values='counts',
+                 color='counts',
                  color_continuous_scale='RdBu',
-                 title='Распределение диабета в зависимости от пола и возраста',
+                 title='Распределение диабета, сердечных заболеваний и гипертонии в зависимости от пола и возраста',
                  template="plotly_dark")
+
+# Показываем фигуру
 fig.show()
